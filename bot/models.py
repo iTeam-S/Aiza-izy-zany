f3b7ea1a41e1cc5e2b570e4a011766e5fcbc7ddc
SergioDev22 <louis.sergio.rakotoarisoa@esti.mg>
2022-07-02 21:46:26 +0300

    [IMP] Make a API_URL dynamic for model

--- bot/models.py
+++ bot/models.py
@@ -1,42 +1,37 @@
 import requests
+from conf import Configuration
+
+API_URL = Configuration.API_URL
 
 
 def get_data_type_de_service(type_de_service):
 
     if type_de_service == "formelle":
-        data = requests.get(
-            "http://127.0.0.1:8000/api/service/?type_de_service=1&active=true"
-        )
+        data = requests.get(API_URL + "/service/?type_de_service=1&active=true")
 
     else:
-        data = requests.get(
-            "http://127.0.0.1:8000/api/service/?type_de_service=2&active=true"
-        )
+        data = requests.get(API_URL + "/service/?type_de_service=2&active=true")
 
     return data.json()
 
 
 def get_categorie():
-    return requests.get("http://127.0.0.1:8000/api/categorie").json()
+    return requests.get(API_URL + "/categorie").json()
 
 
 def get_service(type_de_service, categorie):
     return requests.get(
-        f"http://127.0.0.1:8000/api/service/?categorie={categorie}&type_de_service={type_de_service}&active=true"
+        f"{API_URL}/service/?categorie={categorie}&type_de_service={type_de_service}&active=true"
     ).json()
 
 
 def get_info_service(service_id):
-    return requests.get(f"http://127.0.0.1:8000/api/service/{service_id}").json()
+    return requests.get(f"{API_URL}/service/{service_id}").json()
 
 
 def recherche(mot_cle):
-    return requests.get(
-        f"http://127.0.0.1:8000/api/service/?search={mot_cle}&active=true"
-    ).json()
+    return requests.get(f"{API_URL}/service/?search={mot_cle}&active=true").json()
 
 
 def super_class(classe):
-    return requests.get(
-        f"http://127.0.0.1:8000/api/service/?classe={classe}&active=true"
-    ).json()
+    return requests.get(f"{API_URL}/service/?classe={classe}&active=true").json()
